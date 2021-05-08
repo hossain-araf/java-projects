274157e13a7482de35b4cd360cb0927b5c5e2155
hossain-araf <83327868+hossain-araf@users.noreply.github.com>
2021-05-08 16:31:45 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int m=2;//this is a int
+        int s=m/2;
+        double h=12333.56;//this is a double
+        char a='w';//this only has a charactor
+        byte r=-128;//this is in a byte form
+        boolean isaged=false;// this indicates to true or false only
+        String goat= "i am learning java";
+        System.out.println("hello my name is araf hossain"+m+s);
+        System.out.println(a);
+        System.out.print(h*12);
+        System.out.print(r);
+        System.out.println(goat);
+
+    }
+}
