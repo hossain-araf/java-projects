70ed4f94cb0f64a3a61847cc5c1ded053873beb3
hossain-araf <83327868+hossain-araf@users.noreply.github.com>
2021-09-01 15:58:38 +0600

    Add files via upload

---
+++ multidimensionarrays.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class multidimensionarrays {
+
+    public static void main(String[]args){
+        int [][] numbers={{1,2,3,4,4,5,5,5},{2,3,4,5,6,7,8,7}};
+
+        System.out.print(Arrays.deepToString(numbers));
+
+
+
+    }
+    
+}
