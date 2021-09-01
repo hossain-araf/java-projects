f3cad95bda2788de0644fb7bc801332d869131c3
hossain-araf <83327868+hossain-araf@users.noreply.github.com>
2021-09-01 15:41:25 +0600

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class arrays {
+    public static void main(String[]args){
+        boolean [] numbers = new boolean[12];
+        int [] number = new int[10];
+        number[9] =45;
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(number));
+
+
+
+
+    }
+    
+}
